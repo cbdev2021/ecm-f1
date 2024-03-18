fd5bc7a1292528d185a4e830baac68d81d2d31c0
carlos <cbdev2021@gmail.com>
2024-03-18 02:01:36 -0300

    filtro desde menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
 import Drawer from './components/Drawer';
 import Header from './components/Header';
 import { useState } from 'react';
-import ProductCatalog from './components/ProductCatalog'; // Importar el componente Products
+import ProductCatalog from './components/ProductCatalog'; // Importa el componente ProductCatalog
 import Footer from './components/Footer';
 
 function App() {
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const [categoryFilter, setCategoryFilter] = useState('All'); // Agrega estado para la categoría seleccionada
+
   const toggleDrawer = () => {
     setDrawerOpen(!drawerOpen);
+  };
+
+  const handleCategorySelect = (category: string) => {
+    setCategoryFilter(category); // Actualiza la categoría seleccionada
+    setDrawerOpen(false); // Cierra el Drawer después de seleccionar una categoría
   };
 
   return (
     <>
       <Header onMenuClick={toggleDrawer} />
-      <Drawer open={drawerOpen} onClose={toggleDrawer} onOpen={() => { }} />
-
-      {/* Agregar el componente Products aquí */}
-      <ProductCatalog /> 
-        
+      <Drawer open={drawerOpen} onClose={toggleDrawer} onOpen={() => {}} handleCategorySelect={handleCategorySelect} /> {/* Pasa la función de selección de categoría */}
+      <ProductCatalog categoryFilter={categoryFilter} /> {/* Pasa la categoría seleccionada como prop al componente ProductCatalog */}
       <Footer />
     </>
   );
 }
 
 export default App;
+
+
+// import Drawer from './components/Drawer';
+// import Header from './components/Header';
+// import { useState } from 'react';
+// import ProductCatalog from './components/ProductCatalog'; // Importar el componente Products
+// import Footer from './components/Footer';
+
+// function App() {
+//   const [drawerOpen, setDrawerOpen] = useState(false);
+//   const toggleDrawer = () => {
+//     setDrawerOpen(!drawerOpen);
+//   };
+
+//   return (
+//     <>
+//       <Header onMenuClick={toggleDrawer} />
+//       <Drawer open={drawerOpen} onClose={toggleDrawer} onOpen={() => { }} />
+
+//       {/* Agregar el componente Products aquí */}
+//       <ProductCatalog /> 
+        
+//       <Footer />
+//     </>
+//   );
+// }
+
+// export default App;
--- src/components/ProductCatalog.tsx
+++ src/components/ProductCatalog.tsx
@@ -1,9 +1,25 @@
-import { Grid } from '@mui/material'; // Importamos Grid de Material-UI
-import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from 'react';
+import React, { useState, useEffect, Key } from 'react';
+import { Grid } from '@mui/material';
 import { useGetAllProductsQuery } from '../slices/productsApiSlice';
 
-const ProductCatalog = () => {
+interface ProductCatalogProps {
+  categoryFilter: string; // Define la prop categoryFilter
+}
+
+const ProductCatalog: React.FC<ProductCatalogProps> = ({ categoryFilter }) => {
   const { data: products, error, isLoading } = useGetAllProductsQuery(null);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    if (products) {
+      if (categoryFilter === 'All') {
+        setFilteredProducts(products);
+      } else {
+        const filtered = products.filter((product: { category: string }) => product.category === categoryFilter);
+        setFilteredProducts(filtered);
+      }
+    }
+  }, [categoryFilter, products]);
 
   if (isLoading) return <p>Loading...</p>;
   if (error) {
@@ -17,11 +33,11 @@ const ProductCatalog = () => {
 
   return (
     <Grid container spacing={2} className="grid-container">
-      {products.map((product: { id: Key | null | undefined; image: string | undefined; title: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; price: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; category: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; rating: { rate: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; count: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }; }) => (
+      {filteredProducts.map((product: { id: Key | null | undefined; image: string | undefined; title: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; price: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; category: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; rating: { rate: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; count: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; }; }) => (
         <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
           <div className="product">
-            <div className="image-container"> 
-              <img src={product.image} className="product-image" />
+            <div className="image-container">
+              <img src={product.image} className="product-image" alt="Product" />
             </div>
             <div className="product-details">
               <h2>{product.title}</h2>
@@ -37,3 +53,189 @@ const ProductCatalog = () => {
 };
 
 export default ProductCatalog;
+
+
+// import React, { useState, useEffect, Key } from 'react';
+// import { Grid } from '@mui/material';
+// import { useGetAllProductsQuery } from '../slices/productsApiSlice';
+// import Drawer from './Drawer'; // Importa el componente Drawer
+
+// const ProductCatalog = () => {
+//   const { data: products, error, isLoading } = useGetAllProductsQuery(null);
+//   const [categoryFilter, setCategoryFilter] = useState('All');
+//   const [filteredProducts, setFilteredProducts] = useState([]);
+//   const [drawerOpen, setDrawerOpen] = useState(false); // Agrega estado para controlar la apertura/cierre del Drawer
+
+//   useEffect(() => {
+//     if (products) {
+//       if (categoryFilter === 'All') {
+//         setFilteredProducts(products);
+//       } else {
+//         const filtered = products.filter((product: { category: string }) => product.category === categoryFilter);
+//         setFilteredProducts(filtered);
+//       }
+//     }
+//   }, [categoryFilter, products]);
+
+//   const handleCategorySelect = (category: string) => {
+//     setCategoryFilter(category); // Actualiza la categoría seleccionada
+//     setDrawerOpen(false); // Cierra el Drawer después de seleccionar una categoría
+//   };
+
+//   if (isLoading) return <p>Loading...</p>;
+//   if (error) {
+//     if ('status' in error) {
+//       return <p>Error: {error.status}</p>;
+//     }
+//     if ('data' in error) {
+//       return <p>Error: {String(error.data)}</p>;
+//     }
+//   }
+
+//   return (
+//     <>
+//       <Drawer
+//         open={drawerOpen}
+//         onClose={() => setDrawerOpen(false)}
+//         onOpen={() => setDrawerOpen(true)}
+//         handleCategorySelect={handleCategorySelect} // Pasa la función de selección de categoría al Drawer
+//       />
+//       <select value={categoryFilter} onChange={(e) => setCategoryFilter(e.target.value)}>
+//         <option value="All">All Categories</option>
+//         <option value="Home">Home</option>
+//         <option value="electronics">Electronics</option>
+//         <option value="jewelery">Jewelry</option>
+//         <option value="men's clothing">Men's Clothing</option>
+//         <option value="women's clothing">Women's Clothing</option>
+//       </select>
+//       <Grid container spacing={2} className="grid-container">
+//         {filteredProducts.map((product: { id: Key | null | undefined; image: string | undefined; title: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; price: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; category: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; rating: { rate: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; count: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; }; }) => (
+//           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+//             <div className="product">
+//               <div className="image-container">
+//                 <img src={product.image} className="product-image" alt="Product" />
+//               </div>
+//               <div className="product-details">
+//                 <h2>{product.title}</h2>
+//                 <p>Price: ${product.price}</p>
+//                 <p>Category: {product.category}</p>
+//                 <p>Rating: {product.rating.rate} ({product.rating.count} reviews)</p>
+//               </div>
+//             </div>
+//           </Grid>
+//         ))}
+//       </Grid>
+//     </>
+//   );
+// };
+
+// export default ProductCatalog;
+
+
+// import  { useState, useEffect, Key } from 'react';
+// import { Grid } from '@mui/material';
+// import { useGetAllProductsQuery } from '../slices/productsApiSlice';
+
+// const ProductCatalog = () => {
+//   const { data: products, error, isLoading } = useGetAllProductsQuery(null);
+//   const [categoryFilter, setCategoryFilter] = useState('All');
+//   const [filteredProducts, setFilteredProducts] = useState([]);
+
+//   useEffect(() => {
+
+
+//     if (products) {
+//       if (categoryFilter === 'All') {
+//         setFilteredProducts(products);
+//       } else {
+//         const filtered = products.filter((product: { category: string; }) => product.category === categoryFilter);
+//         setFilteredProducts(filtered);
+//       }
+//     }
+//   }, [categoryFilter, products]);
+
+//   if (isLoading) return <p>Loading...</p>;
+//   if (error) {
+//     if ('status' in error) {
+//       return <p>Error: {error.status}</p>;
+//     }
+//     if ('data' in error) {
+//       return <p>Error: {String(error.data)}</p>;
+//     }
+//   }
+
+//   return (
+//     <>
+//       <select value={categoryFilter} onChange={e => setCategoryFilter(e.target.value)}>
+//         <option value="All">All Categories</option>
+//         <option value="Home">Home</option>
+//         <option value="electronics">Electronics</option>
+//         <option value="jewelery">Jewelry</option>
+//         <option value="men's clothing">Men's Clothing</option>
+//         <option value="women's clothing">Women's Clothing</option>
+//       </select>
+//       <Grid container spacing={2} className="grid-container">
+//         {filteredProducts.map((product: { id: Key | null | undefined; image: string | undefined; title: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; price: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; category: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; rating: { rate: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; count: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }; }) => (
+//           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+//             <div className="product">
+
+//               <div className="image-container">
+//                 <img src={product.image} className="product-image" />
+//               </div>
+//               <div className="product-details">
+//                 <h2>{product.title}</h2>
+//                 <p>Price: ${product.price}</p>
+//                 <p>Category: {product.category}</p>
+//                 <p>Rating: {product.rating.rate} ({product.rating.count} reviews)</p>
+//               </div>
+//             </div>
+//           </Grid>
+//         ))}
+//       </Grid>
+//     </>
+//   );
+// };
+
+// export default ProductCatalog;
+
+
+
+// import { Grid } from '@mui/material'; // Importamos Grid de Material-UI
+// import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from 'react';
+// import { useGetAllProductsQuery } from '../slices/productsApiSlice';
+
+// const ProductCatalog = () => {
+//   const { data: products, error, isLoading } = useGetAllProductsQuery(null);
+
+//   if (isLoading) return <p>Loading...</p>;
+//   if (error) {
+//     if ('status' in error) {
+//       return <p>Error: {error.status}</p>;
+//     }
+//     if ('data' in error) {
+//       return <p>Error: {String(error.data)}</p>;
+//     }
+//   }
+
+//   return (
+//     <Grid container spacing={2} className="grid-container">
+//       {products.map((product: { id: Key | null | undefined; image: string | undefined; title: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; price: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; category: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; rating: { rate: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; count: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }; }) => (
+//         <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+//           <div className="product">
+//             <div className="image-container">
+//               <img src={product.image} className="product-image" />
+//             </div>
+//             <div className="product-details">
+//               <h2>{product.title}</h2>
+//               <p>Price: ${product.price}</p>
+//               <p>Category: {product.category}</p>
+//               <p>Rating: {product.rating.rate} ({product.rating.count} reviews)</p>
+//             </div>
+//           </div>
+//         </Grid>
+//       ))}
+//     </Grid>
+//   );
+// };
+
+// export default ProductCatalog;
